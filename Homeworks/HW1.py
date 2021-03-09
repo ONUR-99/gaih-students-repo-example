cb061c954e40d9e8f3000cc2c86395d43ea1ad4c
onur <76125591+ONUR-99@users.noreply.github.com>
2021-03-09 21:59:49 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,12 @@
-#Explain your work
+list1 = [1,3,5,7,9] #tek sayılar 
+list2 = [0,2,4,6,8] #çift sayılar
 
-#Question 1
-for x in range(a):
-	print(a)
+newlist1 = list1 + list2   #birleşmiş liste
+print(newlist1)
+
+newlist2 = [i * 2 for i in newlist1] #listedeki her bir verinin ikiyle çarpılması 
+print(newlist2)
+
+for i in newlist2:
+    print(i,"veri tipi :", type(i)) 
+    i = i + 1 
