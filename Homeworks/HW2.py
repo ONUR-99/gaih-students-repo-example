be5a8245d1c227cc4b95ea1da7193bfa405a7075
onur <76125591+ONUR-99@users.noreply.github.com>
2021-03-10 21:53:59 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,46 @@
-#Explain your work
+CV_1 = {
+"isim" : "onur",
+"soyisim" : "yıldız",
+"okul" : "kocaeli üniversitesi",
+"bölüm" : "harita mühendisliği",
+"dil" : "ingilizce"
+}
+CV_2 = {
+"isim" : "onur",
+"soyisim" : "yıldız",
+"okul" : "kocaeli üniversitesi",
+"bölüm" : "harita mühendisliği",
+"dil" : "ingilizce"
+}
+CV_3 = {
+"isim" : "onur",
+"soyisim" : "yıldız",
+"okul" : "kocaeli üniversitesi",
+"bölüm" : "harita mühendisliği",
+"dil" : "ingilizce"
+}
+CV_4 = {
+"isim" : "onur",
+"soyisim" : "yıldız",
+"okul" : "kocaeli üniversitesi",
+"bölüm" : "harita mühendisliği",
+"dil" : "ingilizce"
+}
+CV_5 = {
+"isim" : "onur",
+"soyisim" : "yıldız",
+"okul" : "kocaeli üniversitesi",
+"bölüm" : "harita mühendisliği",
+"dil" : "ingilizce"
+}
 
-#Question 1
-for x in range(a):
-	print(a)
+print("CV_1 \n", "isim :" , CV_1["isim"],"\n", "soyisim : " , CV_1["soyisim"] ,"\n","okul : " , CV_1["okul"],"\n",
+"bölüm : " , CV_1["bölüm"],"\n","dil : " , CV_1["dil"],"\n" )
+print("CV_2 \n", "isim :" , CV_1["isim"],"\n", "soyisim : " , CV_1["soyisim"] ,"\n","okul : " , CV_1["okul"],"\n",
+"bölüm : " , CV_1["bölüm"],"\n","dil : " , CV_1["dil"],"\n" )
+print("CV_3 \n", "isim :" , CV_1["isim"],"\n", "soyisim : " , CV_1["soyisim"] ,"\n","okul : " , CV_1["okul"],"\n",
+"bölüm : " , CV_1["bölüm"],"\n","dil : " , CV_1["dil"],"\n" )
+print("CV_4 \n", "isim :" , CV_1["isim"],"\n", "soyisim : " , CV_1["soyisim"] ,"\n","okul : " , CV_1["okul"],"\n",
+"bölüm : " , CV_1["bölüm"],"\n","dil : " , CV_1["dil"],"\n" )
+print("CV_5 \n", "isim :" , CV_1["isim"],"\n", "soyisim : " , CV_1["soyisim"] ,"\n","okul : " , CV_1["okul"],"\n",
+"bölüm : " , CV_1["bölüm"],"\n","dil : " , CV_1["dil"],"\n" )
